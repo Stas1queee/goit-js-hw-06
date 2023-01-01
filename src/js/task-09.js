bfe558c948c74f978401b969c302d74d6ac88719
Stanislav <stas1que@ukr.net>
2023-01-02 00:41:21 +0100

    9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+let body = document.querySelector('body');
+let changeColorBtn = document.querySelector('.change-color');
+let colorValue = document.querySelector('.color');
+
+changeColorBtn.addEventListener('click', changeColor);
+
+function changeColor() {
+  body.style.backgroundColor = getRandomHexColor();
+  colorValue.textContent = getRandomHexColor();
+}
